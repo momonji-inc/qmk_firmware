e2eed887c5b04aedf3ef71149e449ded526fba77
ChrisChrisLoLo <sporewoh@gmail.com>
2023-07-23 18:28:36 -0500

    f

--- keyboards/sporewoh/bancouver40/config.h
+++ keyboards/sporewoh/bancouver40/config.h
@@ -19,6 +19,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xBEAF
+#define PRODUCT_ID      0x0003
+#define DEVICE_VER      0x0000
+#define MANUFACTURER    sporewoh
+#define PRODUCT         bancouver40
+
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 10
